107b696509509d62bcbeb20dc8145c220a946708
danny2590 <dj.encarnacion@gmail.com>
2017-11-13 02:25:22 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,167 @@
+  $.get( "../data/zipcodes.json", function( data ) {
+  console.log($("#myChart"));
+  const myChart = document.getElementById("myChart").getContext("2d");
+
+  //sort through my json data which has created an array of objects and return the listed parameters
+  const neighborhoods = data.map(function(location) {
+    return location.neighborhoods;
+  });
+  const nightlyCost = data.map(function(location) {
+    return location.avgPrice;
+  });
+  const bookingFrequency = data.map(function(location){
+    return location.listings;
+  });
+
+  const chartOptions = {
+    type:'bar', // bar, horizontalBar, pie, line, doughnut, radr, polarArea charts are available
+    data:{
+      labels: neighborhoods,
+      datasets:[
+        {
+       label: 'Price Per Night',
+        data: nightlyCost,
+        backgroundColor: randomColor(),   //this randomColor() method generates a unique random color for my bar-chart
+      },
+       {
+        type: 'line',
+        label: "Booking Frequency",
+        data: bookingFrequency,
+        fill: false,
+
+      }
+    ]
+  },
+    options: {    //The code immediately preceeding options creates adds stylistic elements to my charts
+      legend: { display: true},
+      title: {
+        display: true,
+        text: 'Average Price Per Night'
+      }
+    },  animation: {
+            onProgress: function(animation) {
+                progress.value = animation.animationObject.currentStep / animation.animationObject.numSteps;
+            }
+    }
+  };
+
+  return new Chart(myChart, chartOptions);
+  });
+
+
+
+  //Pie Chart
+  console.log($("#pieChart"));
+  $.get( "../data/zipcodes.json", function( data ) {
+  const pieChart = document.getElementById("pieChart").getContext('2d');
+
+  //sort through my json data which has created an array of objects and return the listed parameters
+  const neighborhoods = data.map(function(location) {
+    return location.neighborhoods;
+  });
+  const nightlyCost = data.map(function(location) {
+    return location.avgPrice;
+  });
+  const bookingFrequency = data.map(function(location){
+    return location.listings;
+  });
+
+  const pieChartOptions = {
+    type:'pie', // bar, horizontalBar, pie, line, doughnut, radr, polarArea charts are available
+    data:{
+      labels: neighborhoods.slice(0, 10),   // As the data (number of neighborhoods && listings) is too large to display in an appealing way
+      backgroundColorHover: randomColor(),  // I am using the slice method to limit my chart to show only the first 10 properties of my neighborhoods array
+      datasets:[{
+        label: "No. of Listings",
+        data: bookingFrequency.slice(0, 10),
+        backgroundColor: randomColor({      //the use of this randomColor call was to create some differentiation in the data by
+            count: 10,                      //pasing an array of colorsinto the diferent properties of my pie-chart
+            hue: 'random',
+            luminosity: 'light',
+        }),
+      }]
+    },
+    options: {
+      legend: {display: true},
+      title: {
+        display: true,
+        text: 'Most Frequently Booked Neighborhoods'
+      }
+    }
+  };
+
+  return new Chart(pieChart, pieChartOptions);
+});
+
+
+// Last Chart - Bedrooms vs. Reviews -- For this chart I am grouping together two sets of data to form a grouped bar-chart
+// as well as adding an overlayed line chart to show progression_______________________
+console.log($("#reviewChart"));
+$.get( "../data/listings.json", function( data ) {
+const reviewChart = document.getElementById("reviewChart").getContext("2d");
+
+                                                    //sort through my json data which has created an array of objects and returned the listed items
+  const bedrooms = data.map(function(location) {      //created a function that sorts through the array and groups the # of bedrooms
+    return location.bedrooms;
+  });
+  const reviewScore = data.map(function(location) {
+    return location.avg_review_score;
+  });
+  const avgEarnings = data.map(function(location){
+    return location.avg_price;
+  });
+  const reviewNums = data.map(function(location){
+    return location.num_reviews;
+  });
+
+  // Bar Chart, Display Options: Color and Labels
+  const plotOptions = {
+    type:'bar', // Types of possible charts as provided by Chart.js bar, horizontalBar, pie, line, doughnut, radr, polarArea charts are available
+    data: {
+      labels: ["Studio", "One Bedroom", "Two Bedroom", "Three Bedroom", "Four Bedroom", "Five Bedroom", "Six Bedroom", "Seven Bedroom"],
+      datasets:[{
+          label: "Per Night Price",
+          type: "line",
+          backgroundColor: "rgba(95, 125, 151, 0.9)",
+          data: avgEarnings,
+          fill: false
+        }, {
+          label: "Average No of Reviews",
+          type: "bar",
+          backgroundColor: "rgba(164, 189, 134, 0.9)",
+          backgroundColorHover: "#3e95cd",
+          data: reviewNums,
+          fill: false
+        }, {
+          label: "Review Score",
+          type: "bar",
+          backgroundColor: "red",
+          data: reviewScore,
+        }]
+      },
+    /*
+      A grouped bar chart to display the correlation of reviews, and price, vs. homeSize for profitability!
+    */
+    options: {
+      legend: { display: true},
+      title: {
+        display: true,
+        text: 'Home Size Vs. Popularity'
+      }, scales: {
+        yAxes: [{
+          scaleLabel:{
+            display: true,
+            labelString: "Avg Price Per Room"
+          }
+        }],
+        xAxes: [{
+          scaleLabel:{
+            display: true,
+            labelString: "No. of Bedrooms"
+          }
+        }]
+      }
+    }
+  };
+  return new Chart(reviewChart, plotOptions);
+  });
